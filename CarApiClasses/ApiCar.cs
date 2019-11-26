2f1f09be4b186ba794394b0d31f0cb9793728490
Horia Cristian Sandu <horiasandu1@gmail.com>
2019-11-26 09:54:24 -0500

    Add project files.

---
+++ CarApiClasses/ApiCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApiClasses
+{
+    public class ApiCar
+    {
+        public int CarId { get; set; }
+
+        public string CarColor { get; set; }
+
+    }
+}
---
+++ CarSalesApi/Models/DBAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarApiClasses;
+
+namespace CarSalesApi.Models
+{
+    public class DBAccess
+    {
+
+        private CarSalesDBEntities db = new CarSalesDBEntities();
+
+        public List<ApiCar> GetCars()
+        {
+            // Get from EF
+
+            var cars = db.Cars.ToList();
+
+            // convert to api list see my git example
+
+            //return cars;
+
+            List < ApiCar > newCarList = cars.Select(x => new ApiCar() { CarId = x.CarId }).ToList();
+
+
+
+            return newCarList;
+
+
+        }
+
+
+
+
+    }
+}
