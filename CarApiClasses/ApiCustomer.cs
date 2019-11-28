33f31939c67bb652121fa1b99f444ca63a400671
Karim Ibrahim <40270714+chillman12@users.noreply.github.com>
2019-11-27 23:35:51 -0500

    Added The Car Api Classes

---
+++ CarApiClasses/ApiCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApiClasses
+{
+    public class ApiCustomer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public int CustomerPhoneId { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerDOB { get; set; }
+    }
+}
---
+++ CarApiClasses/ApiSale.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApiClasses
+{
+    class ApiSale
+    {
+        public int SaleId { get; set; }
+        public int CustimerId { get; set; }
+        public int CarId { get; set; }
+        public int SalesPersonId { get; set; }
+        public string SaleDate { get; set; }
+        public double SaleTotal { get; set; }
+        public int SaleQuantity { get; set; }
+
+    }
+}
